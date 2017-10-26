7a09b75ce26b666e5579ba2771f4453b080b6de0
chenxuanfeng <chenxuanfeng@chenxuanfengdeMacBook-Pro.local>
2017-10-26 21:27:14 +0800

    修改同构代码，优化代码调试方式

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const open = require("open");
 const render = require('./dist/index.js')
 
 const app = new Koa();
 const router = new Router();
+const port = 3000;
+const host = '127.0.0.1';
+
 
 router.get(/^\/.*/,
     render.default
 );
 app.use(router.routes());
-app.listen(3000);
+app.listen(port);
 
 console.log('开始监听：');
-
+const url = 'http://' + host + ':' + port + '/home';
+console.log('打开默认页面: ' + url);
+open(url);
 
--- platforms/router/reactRouter.js
+++ platforms/router/reactRouter.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
-//import createHistory from 'history/createHashHistory';
+import createHistory from 'history/createHashHistory';
 import Header from '../../src/component/header/header.jsx';
 import Home from '../../src/component/home/home.jsx';
 import Email from '../../src/component/email/email.jsx';
 import Me from '../../src/component/me/me.jsx';
 
 
-//const history = createHistory();
+const history = createHistory();
 
 export default (
-    <Router>
+    <Router history={history}>
         <div className = "blogbox">
             <Route exact path="/home" component = {Home} ></Route>
             <Route exact path="/email" component = {Email} ></Route>
@@ -20,11 +20,3 @@ export default (
 )
 
 
-
-// <Router history={history}>
-//         <div className = "blogbox">
-//             <Route exact path="/home" component = {Home} ></Route>
-//             <Route exact path="/email" component = {Email} ></Route>
-//             <Route exact path="/me" component = {Me} ></Route>
-//         </div>
-//     </Router>

9ff9203fb4a3c867e8785640a691a19520b50e8b
574322189@qq.com <574322189@qq.com>
2018-01-24 17:46:23 +0800

    add

--- platforms/server/index.js
+++ platforms/server/index.js
@@ -8,6 +8,16 @@ import reducers from '../../src/redux/reducer';
 import { layout } from './layout.js';
 import router from './router.js';
 
+
+import Header from '../../src/component/header/header.jsx';
+import Home from '../../src/component/home/home.jsx';
+import Email from '../../src/component/email/email.jsx';
+import Me from '../../src/component/me/me.jsx';
+import List from '../../src/component/list/list.jsx';
+import Search from '../../src/component/search/search.jsx';
+import Detail from '../../src/component/detail/detail.jsx';
+
+
 const initialState = {}
 const middleware = [thunk]
 const finalCreateStore = applyMiddleware(...middleware)(createStore)
@@ -43,13 +53,21 @@ export default async function (ctx) {
   const initData = store.getState()
   const html = layout(renderToString(
     <Provider store={store}>
-      <MemoryRouter location={ctx.url}>
-        <div className="blogbox">
-          {
-            commponentAry.map(Item => {
-              return <Item />
-            })
-          }
+      <MemoryRouter>
+        <div className = "blogbox">
+            <Header/>
+            <Switch>
+                <Route path="/" exact component = {Home} ></Route>
+                <Route path="/index.html" exact component = {Home} ></Route>
+                <Route path="/m/index.html" exact component = {Home} ></Route>
+                <Route path="/home" component = {Home} ></Route>
+                <Route path="/list" component = {List} ></Route>
+                <Route path="/search" component = {Search}></Route>
+                <Route path="/detail/:id" component = {Detail} ></Route>
+                <Route path="/me" component = {Me} ></Route>
+                <Route path="/email" component = {Email} ></Route>
+                <Redirect to="/m/index.html" />
+            </Switch>
         </div>
       </MemoryRouter>
     </Provider>
